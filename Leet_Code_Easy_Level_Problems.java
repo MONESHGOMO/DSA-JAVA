bb0fce2a2b32de4d5e7120e57f4e74bdf1017f34
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-16 20:39:50 +0530

    Leet code  easy problem

---
+++ Leet_Code_Easy_Level_Problems.java
@@ -0,0 +1,22 @@
+package GOMO_JAVA_CODES;
+
+public class Leet_Code_Easy_Level_Problems {
+
+	public static void main(String[] args) {
+          
+		/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+		
+		Example 1:
+
+		Input: nums = [2,7,11,15], target = 9
+		Output: [0,1]
+		Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+        */
+		
+		int num[] = {23,40,20,12};
+		int target=60;
+		 
+		System.out.println(num[1] + num[2]);
+	}
+
+}
