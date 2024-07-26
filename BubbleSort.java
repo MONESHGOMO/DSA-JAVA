16756f7af69c9b7f0dcce8fe43cf7e71279393de
MONESH <167512723+MONESHGOMO@users.noreply.github.com>
2024-07-26 20:45:43 +0530

    adding notes

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,7 +2,25 @@ package GOMO_JAVA_CODES;
 import java.util.Arrays;
 public class BubbleSort {
 
+
+	
+/*
+ * 1) comparing the adjacent element if(1st element is greater than the second element ) if condition true swap ,sequential implementation
+ * 2)[imp] why bubble sort : Bubble sort is easy to understand and implement. It does not require any additional memory space.
+ * 3)[imp] another name of bubble sort is { sinking sort }  or { in-place sorting } algorithm (which means no additional memory required )
+ * 4)bubble sort (comparison sorting method )
+ * 5)Time complexity :  [ O(n) in best case , O(n^2) in the worst case ] and space complexity : O(n) 
+ * 6)[imp]Is Bubble Sort Stable? Yes, Bubble Sort is a stable sorting algorithm.
+ *  We swap elements only when A is less than B. If A is equal to B,
+ *  we do not swap them, hence relative order between equal elements will be maintained.
+ * 7) bubble sort is a stable algorithm
+ * 
+ */
+    
+
 	public static void main(String[] args) {
+
+	
 
 	int a[]= {354,-3,-5,3543,567,-3,-34,23};// unsorted array
 	bubblesort(a);
