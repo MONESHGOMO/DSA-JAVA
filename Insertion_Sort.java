729044c5e1b0feda8a86782d8ab56a5ef6e274ea
GOMO_PROGRAMMER <167512723+MONESHGOMO@users.noreply.github.com>
2024-08-07 20:39:05 +0530

    feat : adding  i  loop comment

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -24,6 +24,7 @@ public class Insertion_Sort {
        static void insertionSort(int[] a) {
     	   	
     	   for(int i=0;i<a.length-1;i++) { // range: i<a.length-1 or i<=a.length-2 
+		// i loop runs from zero to (n-2) 
     		    
     		   for(int j=i+1;j>0;j--) {
     			   if(a[j]<a[j-1]) {
