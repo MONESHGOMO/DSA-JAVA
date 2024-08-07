fe6ec1a17bfa5bd6700b1a115dc92c60d9c0097b
GOMO_PROGRAMMER <167512723+MONESHGOMO@users.noreply.github.com>
2024-08-07 20:30:09 +0530

    feat: j loop comment

--- BubbleSort.java
+++ BubbleSort.java
@@ -36,8 +36,8 @@ public class BubbleSort {
 		
 		for (int i = 0; i < a.length; i++) {
 			for (int j = 1; j < a.length-i; j++) {
+				// the j loop runs (n-i) times ,where n is the length of the given array 
 				if( a[j]<a[j-1]) {
-					//swap the elements
 					
 				   int temp=a[j-1];
 				   a[j-1]=a[j];
